39637519dff8cc99e1f667b4dcb7f11b8a3f7219
hari-krishnan427 <harikrishnanboopalan2@gmail.com>
2025-08-07 19:57:16 +0530

    Add files via upload
    Signed-off-by: hari-krishnan427 <harikrishnanboopalan2@gmail.com>

---
+++ script.js
@@ -0,0 +1,27 @@
+
+async function sendMessage() {
+  const input = document.getElementById("user-input");
+  const message = input.value.trim();
+  if (!message) return;
+
+  const chatBox = document.getElementById("chat-box");
+  chatBox.innerHTML += `<div class="message user"><strong>You:</strong> ${message}</div>`;
+  input.value = "";
+  input.disabled = true;
+
+  const response = await fetch('https://harikrishnan22082006.pythonanywhere.com/chat', {
+
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({
+      model: "flipkart-bot",
+      prompt: message,
+      stream: false
+    })
+  });
+
+  const data = await response.json();
+  chatBox.innerHTML += `<div class="message bot"><strong>Bot:</strong> ${data.response}</div>`;
+  chatBox.scrollTop = chatBox.scrollHeight;
+  input.disabled = false;
+}
