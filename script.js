0e8a9ad19c97f37b329b1f42b5ceb736a0557b58
hari-krishnan427 <harikrishnanboopalan2@gmail.com>
2025-08-07 14:04:56 +0530

    Update script.js
    Signed-off-by: hari-krishnan427 <harikrishnanboopalan2@gmail.com>

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ async function sendMessage() {
   input.value = "";
   input.disabled = true;
 
-  const response = await fetch("http://localhost:11434/api/generate", {
+  const response = await fetch("http://customer-supportbot-production.up.railway.app/api/generate", {
 ";, {
     method: "POST",
     headers: { "Content-Type": "application/json" },
